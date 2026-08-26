4a00d822a47d82c834712bf1f60feb8ac0462283
kevinstiebich <kevin.stiebich@gmail.com>
2026-08-26 11:02:55 +0200

    funktion auslagern und while schleife beginnen

--- compress.dart
+++ compress.dart
@@ -4,15 +4,51 @@ void main(List<String> args) {
   String toCompress;
 
   if (args.isEmpty) {
-    print("Gebe etwas ein: ");
+    print("Eingabe: ");
     toCompress = stdin.readLineSync()!;
   } else {
     toCompress = args[0];
   }
 
+  getValidFormat(toCompress);
+
+  var charAmount = toCompress.length;
+  List<String> singleChars = toCompress.split("");
+  var charCounter = 1;
+  var i = 1;
+
+  while (i < charAmount) {
+
+    if (singleChars[i - 1] == singleChars[i]) {
+      charCounter++;
+      i++;
+    }
+
+    if (charCounter >= 3 && (singleChars[i - 1] != singleChars[i] || i == charAmount - 1)) {
+      singleChars.replaceRange((i - charCounter) + 1, i, ["$charCounter"]);
+      charAmount -= (charCounter - 1);
+      i -= (charCounter - 1);
+      charCounter = 1;
+      i++;
+      continue;
+    } 
+
+    if (singleChars[i - 1] != singleChars[i]) {
+      charCounter = 1;
+      i++;
+    }
+
+  }
+
+  toCompress = singleChars.join("");
+  print("$toCompress");
+}
+
+String getValidFormat(toCompress) {
   while (!RegExp(r'^[a-zA-Z]+$').hasMatch(toCompress)) {
     print("Fehler: Die Eingabe darf nur Buchstaben erhalten.");
-    print("Gebe etwas ein: ");
+    print("Eingabe: ");
     toCompress = stdin.readLineSync()!;
   }
+  return toCompress;
 }
