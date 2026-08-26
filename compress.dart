ea283091e99ff77a86c3d5cbd00d9a6a2573fe32
kevinstiebich <kevin.stiebich@gmail.com>
2026-08-26 13:53:34 +0200

    lösen der Aufgabe

--- compress.dart
+++ compress.dart
@@ -1,8 +1,38 @@
 import "dart:io";
 
 void main(List<String> args) {
-  String toCompress;
+  String toCompress = getValidFormat(args);
+  compressAndPrint(toCompress);
+}
 
+void compressAndPrint(toCompress) {
+  List<String> singleChars = toCompress.split("");
+  var charCounter = 1;
+  var i = 1;
+
+  while (i < singleChars.length) {
+    if (singleChars[i - 1] == singleChars[i]) {
+      charCounter++;
+      i++;
+    } else {
+      if (charCounter >= 3) {
+        singleChars.replaceRange(i - charCounter + 1, i, ["$charCounter"]);
+        i -= charCounter - 2;
+      }
+      charCounter = 1;
+      i++;
+      }
+  }
+
+  if (charCounter >= 3) singleChars.replaceRange(i - charCounter + 1, i, ["$charCounter"]);
+
+  toCompress = singleChars.join("");
+  print("$toCompress");
+}
+
+String getValidFormat(List<String> args) {
+  String toCompress;
+  
   if (args.isEmpty) {
     print("Eingabe: ");
     toCompress = stdin.readLineSync()!;
@@ -10,41 +40,6 @@ void main(List<String> args) {
     toCompress = args[0];
   }
 
-  getValidFormat(toCompress);
-
-  var charAmount = toCompress.length;
-  List<String> singleChars = toCompress.split("");
-  var charCounter = 1;
-  var i = 1;
-
-  while (i < charAmount) {
-
-    if (singleChars[i - 1] == singleChars[i]) {
-      charCounter++;
-      i++;
-    }
-
-    if (charCounter >= 3 && (singleChars[i - 1] != singleChars[i] || i == charAmount - 1)) {
-      singleChars.replaceRange((i - charCounter) + 1, i, ["$charCounter"]);
-      charAmount -= (charCounter - 1);
-      i -= (charCounter - 1);
-      charCounter = 1;
-      i++;
-      continue;
-    } 
-
-    if (singleChars[i - 1] != singleChars[i]) {
-      charCounter = 1;
-      i++;
-    }
-
-  }
-
-  toCompress = singleChars.join("");
-  print("$toCompress");
-}
-
-String getValidFormat(toCompress) {
   while (!RegExp(r'^[a-zA-Z]+$').hasMatch(toCompress)) {
     print("Fehler: Die Eingabe darf nur Buchstaben erhalten.");
     print("Eingabe: ");
